e4cc5c5d7d291443d988dbaa156abf908029c88c
linzd <717537314@qq.com>
2020-01-15 17:08:28 +0800

    代码提交

--- attachment/src/main/java/com/linzd/attachment/AttachmentApplication.java
+++ attachment/src/main/java/com/linzd/attachment/AttachmentApplication.java
@@ -1,9 +1,17 @@
 package com.linzd.attachment;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
+@EnableEurekaClient
+@EnableFeignClients
+@EnableCircuitBreaker
+@MapperScan("com.linzd.*.*.mapper")
 public class AttachmentApplication {
 
     public static void main(String[] args) {
